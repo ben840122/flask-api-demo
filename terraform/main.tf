0af1e4efab8935942943cf2191f4acb625ec956b
benchen <ben840122@gmail.com>
2025-11-05 04:19:53 +0800

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "flask" {
+  metadata {
+    name = "flask-api"
+  }
+}
+
+resource "kubernetes_config_map" "flask-config" {
+  metadata {
+    name      = "flask-config"
+    namespace = kubernetes_namespace.flask.metadata[0].name
+  }
+
+  data = {
+    APP_ENV = "production"
+    DEBUG   = "false"
+  }
+}
+
+resource "kubernetes_ingress_v1" "flask-ingress" {
+  metadata {
+    name      = "flask-api-ingress"
+    namespace = kubernetes_namespace.flask.metadata[0].name
+    annotations = {
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+    }
+  }
+
+  spec {
+    rule {
+      host = "flask.local"
+      http {
+        path {
+          path = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "flask-api-service"
+              port {
+                number = 5000
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
